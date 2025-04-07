01b467875dde20ba25bef805056091ae6e5fb4eb
Igor Augusto <i.augusto@hotmail.com>
2025-04-07 12:44:34 -0300

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>Se as coisas estão difíceis hoje, é porque logo elas vão melhorar. E se está fácil, ou você já chegou lá ou deu um passo a mais rumo ao que quer.</h1>
+    </div>
+  );
+}
+
+export default Home;
