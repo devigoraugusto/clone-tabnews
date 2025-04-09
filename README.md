30f14f90c53513d41bf3308f01fa5b01b5c13781
Igor Augusto <i.augusto@hotmail.com>
2025-04-09 17:11:24 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendizado tabnews do https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 function Home() {
   return (
     <div>
-      <h1>Se as coisas estão difíceis hoje, é porque logo elas vão melhorar. E se está fácil, ou você já chegou lá ou deu um passo a mais rumo ao que quer.</h1>
+      <h1>
+        Se as coisas estão difíceis hoje, é porque logo elas vão melhorar. E se
+        está fácil, ou você já chegou lá ou deu um passo a mais rumo ao que
+        quer.
+      </h1>
     </div>
   );
 }
